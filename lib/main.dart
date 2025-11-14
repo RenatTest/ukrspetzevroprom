d1b9efc91500eda805be39df0d893f4fe73f90e5
RenatTest <renattesttask@gmail.com>
2025-11-14 17:52:17 +0200

    init deploy

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,6 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Ukrspetzevroprom')));
+    return Scaffold(body: Center(child: Text('ukrspetzevroprom')));
   }
 }
