762d489b41e2c3ebe6e010acb204de88d6c85ed1
RenatTest <renattesttask@gmail.com>
2025-11-21 12:15:36 +0200

    feat(USEP-about-us): fix UI

--- lib/features/about_us/presentation/ui/screens/about_us.dart
+++ lib/features/about_us/presentation/ui/screens/about_us.dart
@@ -20,41 +20,54 @@ class AboutUs extends StatelessWidget {
         children: [
           AppBarCustom(),
           Center(
-            child: Column(
-              children: [
-                Text('Про нас'),
-                BlocBuilder<AboutUsCubit, AboutUsState>(
-                  builder: (context, state) {
-                    return switch (state.status) {
-                      AboutUsStatus.initial => const Center(
-                        child: Text(
-                          'About us data',
-                          style: TextStyle(fontSize: 24),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                spacing: 10,
+                children: [
+                  Text('ПРО КОМПАНІЮ', style: TextStyle(fontSize: 24)),
+                  BlocBuilder<AboutUsCubit, AboutUsState>(
+                    builder: (context, state) {
+                      return switch (state.status) {
+                        AboutUsStatus.initial => const Center(
+                          child: Text(
+                            'About us data',
+                            style: TextStyle(fontSize: 24),
+                          ),
                         ),
-                      ),
 
-                      AboutUsStatus.loading => const Center(
-                        child: CircularProgressIndicator(),
-                      ),
-
-                      AboutUsStatus.loaded => Column(
-                        children: [
-                          Text(state.aboutUsInfo.title),
-                          Text(state.aboutUsInfo.description),
-                        ],
-                      ),
-
-                      AboutUsStatus.error => Center(
-                        child: Text(
-                          state.errorMessage ?? 'Error',
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(fontSize: 24),
+                        AboutUsStatus.loading => const Center(
+                          child: CircularProgressIndicator(),
                         ),
-                      ),
-                    };
-                  },
-                ),
-              ],
+
+                        AboutUsStatus.loaded => Column(
+                          spacing: 10,
+                          children: [
+                            Text(
+                              state.aboutUsInfo.title,
+                              style: TextStyle(fontSize: 24),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              state.aboutUsInfo.description,
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+
+                        AboutUsStatus.error => Center(
+                          child: Text(
+                            state.errorMessage ?? 'Error',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                        ),
+                      };
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
           Container(),
